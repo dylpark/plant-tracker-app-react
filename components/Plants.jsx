33c66b44fd0911141f50dc9d4054b2d1388ff277
Dylan Park <dylpark@protonmail.com>
2021-10-30 10:03:25 +1000

    Styling Plants component

--- components/Plants.jsx
+++ components/Plants.jsx
@@ -1,13 +1,23 @@
 //
 //
+import Image from "next/image";
 
 function Plants(props) {
   return (
-    <div>
-      <img src={props.img} />
-      <h2>{props.name}</h2>
-      <h3>{props.species}</h3>
-      <p>{props.wateringInstructions}</p>
+    <div className="p-2">
+      <Image layout="responsive" src={props.img} height={1080} width={1920} />
+
+      <div className="p-2">
+        <h2 className="mt-1 text-2xl text-gray-900 font-semibold">
+          {props.name}
+        </h2>
+
+        <h3 className="mt-1 text-1xl text-gray-600">{props.species}</h3>
+
+        <p className="truncate max-w-md text-gray-900">
+          {props.wateringInstructions}
+        </p>
+      </div>
     </div>
   );
 }
