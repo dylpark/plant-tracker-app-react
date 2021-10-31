69a647544422a23faa5a4c2964184e65269cbd4d
Dylan Park <dylpark@protonmail.com>
2021-10-31 11:30:16 +1000

    Adjusting grid for form component

--- components/form/Form.jsx
+++ components/form/Form.jsx
@@ -28,59 +28,57 @@ export default function Form() {
   // };
 
   return (
-    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
-      <div class="text-center pb-12">
-        <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-10">
+      <div className="text-center pb-12">
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
           Add Your Plant!
         </h1>
       </div>
 
-      {/* Column 1 */}
       <form
         className="mb-0 space-y-6"
         action="#"
         method="POST"
         // onSubmit={savePlant}
-      />
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col md:flex-row">
-          <div class="w-full md:w-2/5 h-80"></div>
-          <div class="w-full md:w-3/5 text-left p-6 md:p-4 space-y-2">
-            <Input
-              for="Name"
-              title="Name"
-              name="name"
-              // onChange={handleChange}
-              // value={plant.name}
-            />
-            <Input
-              for="species"
-              title="Species"
-              name="species"
-              // onChange={handleChange}
-              // value={plant.species}
-            />
-            <FileUpload />
+      >
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 bg-white rounded-lg shadow-lg">
+          {/* Column 1 */}
+          <div className="w-full overflow-hidden flex flex-col md:flex-row">
+            <div className="w-full md:w-3/5 text-left p-6 md:p-4 space-y-2">
+              <Input
+                for="Name"
+                title="Name"
+                name="name"
+                // onChange={handleChange}
+                // value={plant.name}
+              />
+              <Input
+                for="species"
+                title="Species"
+                name="species"
+                // onChange={handleChange}
+                // value={plant.species}
+              />
+              <FileUpload />
 
-            <div class="flex justify-start space-x-2"></div>
+              <div className="flex justify-start space-x-2"></div>
+            </div>
+          </div>
+
+          {/* Column 2 */}
+          <div className="w-full overflow-hidden flex flex-col md:flex-row">
+            <div className="w-full md:w-3/5 text-left p-4 md:p-4 space-y-2">
+              <MarkdownEditor title="Details & Requirements" />
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-800 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              >
+                Submit
+              </button>
+            </div>
           </div>
         </div>
-
-        {/* Column 2 */}
-        <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col md:flex-row">
-          <div class="w-full md:w-2/5 h-80"></div>
-          <div class="w-full md:w-3/5 text-left p-4 md:p-4 space-y-2">
-            <button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-800 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-            >
-              Submit
-            </button>
-
-            <MarkdownEditor title="Details & Requirements" />
-          </div>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
