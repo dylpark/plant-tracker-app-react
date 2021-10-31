4ca7d27b622b66e9f29c50bf4aa4de3a5b50c3ab
Dylan Park <dylpark@protonmail.com>
2021-10-31 12:04:28 +1000

    Creating markdown columns

--- components/form/MarkdownEditor.jsx
+++ components/form/MarkdownEditor.jsx
@@ -6,22 +6,24 @@ function MarkdownEditor(props) {
   const [markdown, setMarkdown] = useState();
 
   return (
-    <div>
-      <label class="text-m font-thin text-gray-700">{props.title}</label>
-      <div className="mt-1">
-        <textarea
-          className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-green-500"
-          autofocus
-          placeholder="Markdown Editor"
-          value={markdown}
-          onChange={(e) => setMarkdown(e.target.value)}
-        />
-
-        <ReactMarkdown
-          children={markdown}
-          className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-green-500"
-        />
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+      <div>
+        <label class="text-m font-thin text-gray-700">{props.title}</label>
+        <div className="mt-1">
+          <textarea
+            className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-green-500"
+            autofocus
+            placeholder="Markdown Editor"
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+          />
+        </div>
       </div>
+
+      <ReactMarkdown
+        children={markdown}
+        className="border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-green-500"
+      />
     </div>
   );
 }
