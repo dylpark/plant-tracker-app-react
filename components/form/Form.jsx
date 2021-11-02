c8595a7845ac4e38fdc4aaedbb5a9dadd7cd0726
Dylan Park <dylpark@protonmail.com>
2021-11-03 09:17:10 +1000

    Creating preview build

--- components/form/Form.jsx
+++ components/form/Form.jsx
@@ -2,58 +2,58 @@
 //
 
 import React from "react";
-import { useState } from "react";
-import axios from "axios";
+// import { useState } from "react";
+// import axios from "axios";
 import TextInput from "./TextInput";
 import FileInput from "./FileInput";
 import InfoInput from "./InfoInput";
 import Button from "./Button";
 
 export default function Form() {
-  const [plantInput, setPlant] = useState({
-    name: "",
-    species: "",
-    // info: "",
-    // image: "",
-    error_list: [],
-  });
+  // const [plantInput, setPlant] = useState({
+  //   name: "",
+  //   species: "",
+  //   // info: "",
+  //   // image: "",
+  //   error_list: [],
+  // });
 
-  const handleChange = (e) => {
-    e.persist();
-    setPlant({ ...plantInput, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (e) => {
+  //   e.persist();
+  //   setPlant({ ...plantInput, [e.target.name]: e.target.value });
+  // };
 
-  const savePlant = (e) => {
-    console.log("hello");
-    e.preventDefault();
+  // const savePlant = (e) => {
+  //   console.log("hello");
+  //   e.preventDefault();
 
-    const data = {
-      name: plantInput.name,
-      species: plantInput.species,
-      // info: plantInput.info,
-      // image: plantInput.image,
-    };
+  //   const data = {
+  //     name: plantInput.name,
+  //     species: plantInput.species,
+  //     // info: plantInput.info,
+  //     // image: plantInput.image,
+  //   };
 
-    axios
-      .post(`http://localhost:8000/api/add-plant`, data)
-      .then((res) => {
-        if (res.data.status === 200) {
-          swal("Success!", res.data.message, "success");
-          setPlant({
-            name: "",
-            species: "",
-            // info: "",
-            // image: "",
-            error_list: [],
-          });
-          history.push("/plants");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        setPlant({ ...plantInput, error_list: err.response.data.validate_err });
-      });
-  };
+  // axios
+  //   .post(`http://localhost:8000/api/add-plant`, data)
+  //   .then((res) => {
+  //     if (res.data.status === 200) {
+  //       swal("Success!", res.data.message, "success");
+  //       setPlant({
+  //         name: "",
+  //         species: "",
+  //         // info: "",
+  //         // image: "",
+  //         error_list: [],
+  //       });
+  //       history.push("/plants");
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     setPlant({ ...plantInput });
+  //     // , error_list: err.response.data.validate_err });
+  //   });
 
   return (
     <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-10">
@@ -63,7 +63,7 @@ export default function Form() {
         </h1>
       </div>
 
-      <form className="mb-0 space-y-6" onSubmit={savePlant}>
+      <form className="mb-0 space-y-6" onSubmit="">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 bg-white rounded-lg shadow-sm">
           {/* Column 1 */}
           <div className="w-full overflow-hidden flex flex-col md:flex-row">
@@ -74,8 +74,8 @@ export default function Form() {
                 key="1"
                 title="Name"
                 name="name"
-                onChange={handleChange}
-                value={plantInput.name}
+                // onChange={handleChange}
+                // value={plantInput.name}
               />
               {/* Species Input */}
               <TextInput
@@ -83,16 +83,16 @@ export default function Form() {
                 key="2"
                 title="Species"
                 name="species"
-                onChange={handleChange}
-                value={plantInput.species}
+                // onChange={handleChange}
+                // value={plantInput.species}
               />
               {/* File Input */}
-              {/* <FileInput
+              <FileInput
                 title="Upload an Image"
                 key="3"
                 name="image"
                 // value={plantInput.image}
-              /> */}
+              />
             </div>
           </div>
 
