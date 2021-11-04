80ad09c089bab4559c2d5f6c5a54c1f96dc97cc9
Dylan Park <dylpark@protonmail.com>
2021-11-04 20:46:57 +1000

    Adjusting form spacing

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -40,10 +40,10 @@ export default function Form() {
       // method="POST" 
       // onSubmit={createPlant}
       >
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 bg-white rounded-lg shadow-sm">
+        <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-5 bg-white rounded-lg shadow-sm">
           {/* Column 1 */}
           <div className="w-full overflow-hidden flex flex-col md:flex-row">
-            <div className="w-full md:w-full text-left p-6 md:p-4 space-y-2">
+            <div className="w-full md:w-full text-left p-4 md:p-4 space-y-2">
               {/* Name Input */}
               <TextInput
                 htmlFor="plant name"
