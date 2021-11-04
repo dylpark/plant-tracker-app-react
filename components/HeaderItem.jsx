280a553d0b633dba56a9ff828e510996edf04d29
Dylan Park <dylpark@protonmail.com>
2021-11-04 20:33:53 +1000

    Adding hover animation to HeaderItem

--- components/HeaderItem.jsx
+++ components/HeaderItem.jsx
@@ -7,7 +7,7 @@ export default function HeaderItem({ Icon, title, href }) {
     <Link href={href} passHref>
       <div className="flex flex-col mr-2 items-center cursor-pointer group w-12 sm:w-20">
         <Icon
-          className="h-8 mb-1 group-hover:animate-bounce text-gray-900"
+          className="h-8 mb-1 transition duration-150 ease-out transform hover:scale-110 text-gray-900"
           href={href}
         />
         <p className="tracking-widest font-thin hover:font-medium text-gray-900 ">
